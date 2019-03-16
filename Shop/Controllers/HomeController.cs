628c5ef4cb43b632f1076f3f2a7a55892b18d81f
shop333 <phkkinz@gmail.com>
2019-03-16 11:08:19 +0700

    first commit

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        ShopEntities db = new ShopEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ViewMenCollection()
+        {
+            
+            return PartialView(db.Products.ToList());
+        }
+    }
+}
